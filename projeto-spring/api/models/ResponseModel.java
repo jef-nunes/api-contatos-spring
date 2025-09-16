fbd2942ab0bf7ba14c9dd8e4ac01ce04fa390032
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-09-16 14:54:32 -0300

    Update ResponseModel.java

--- projeto-spring/api/models/ResponseModel.java
+++ projeto-spring/api/models/ResponseModel.java
@@ -12,13 +12,29 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ResponseModel {
+package com.example.eco.domain.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+// Padronizar o JSON retornado ao cliente
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseModel {
+    private String status;
     private List<?> data;
     private List<?> messages;
     private LocalDateTime timestamp;
 
-    public ResponseModel(List<?> data, LocalDateTime timestamp, List<?> messages) {
+    public ResponseModel(String status, List<?> data, LocalDateTime timestamp, List<?> messages) {
+        this.status = status;
         this.data = data;
         this.timestamp = timestamp;
         this.messages = messages;
